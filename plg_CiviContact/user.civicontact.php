61350c817d8abb4eddf8206bc4d3b4a9376dfd56
combrains <combrains@gmail.com>
2014-02-10 21:33:34 +1300

    added code to get user details from Civi and display them in a CB tab

--- plg_CiviContact/user.civicontact.php
+++ plg_CiviContact/user.civicontact.php
@@ -8,4 +8,36 @@ class getCiviTab extends cbTabHandler {
 		$this->cbTabHandler();
 	}
 
+	function getDisplayTab($tab,$user,$ui) {
+		
+		// Perform Civi bootstrap
+		define('CIVICRM_SETTINGS_PATH', JPATH_ADMINISTRATOR . '/components/com_civicrm/civicrm.settings.php');
+		define('CIVICRM_CORE_PATH', JPATH_ADMINISTRATOR . '/components/com_civicrm/civicrm/');
+		require_once CIVICRM_SETTINGS_PATH;
+		require_once CIVICRM_CORE_PATH .'CRM/Core/Config.php';
+		$config = CRM_Core_Config::singleton();
+		
+		
+		try{
+			$contacts = civicrm_api3('UF_MATCH', 'get', array(
+			'sequential' => 1,
+			));
+			
+			}
+		
+		catch (CiviCRM_API3_Exception $e) {
+			$error = $e->getMessage();
+			
+			}
+		
+		$return = "this is a new tab and the user viewing this tab is " . $user->name . var_dump($user) . 
+		"<br />" . var_dump($contacts) . 
+		"br />" . printf($error);
+		
+	
+	return $return;
+	
+	} //end getDisplayTab function
+}//end getCiviTab class	
+	
 ?>
